4c05f5fe20179ed7db7eac29ba600aafb92becd6
153 <x153@live.com>
2024-06-08 18:32:15 +0800

    further work on page publishing

--- edit.py
+++ edit.py
@@ -6,32 +6,74 @@ from utils import *
 
 edit = Blueprint("edit", __name__)
 
-@edit.route("/e/")
-def new_page():
-    return "Hi"
-
 @edit.route("/e/", methods = ["POST"])
-@edit.route("/e/<page>")
+@edit.route("/e/<page>", methods=["POST", "GET"])
 def page_editor(page=None):
+    author = "Anonymous"
+    title = ""
     content = ""
     data = request.form
+    print(data)
     if request.method == "GET":
         if page_exist(page):
             with open(f"pages/{page}.txt", "r") as content:
                 content = content.read()
+            title = page
+        else:
+            title = fn_check(page)
     else:
         if "title" in data:
-            page = data["title"]
+            title = fn_check(data["title"])
+            print(title)
         if "content" in data:
-            content = data["content"]
+            content = data["content"].strip()
+        if "author" in data:
+            if len(author):
+                author = data["author"]
+        if not "preview" in data:
+            if len(title) and len(content):
+                return publish(title, content, author)
 
     with open("html/edit.html") as temp:
         temp = temp.read()
-    preview = view.format_page(page, content, True)
-    preview = temp.format(preview, content, page)
-    head = "\n".join(page_head(f"Edit: {page}"))
+    preview = view.format_page(title, content, True)
+    preview = temp.format(preview, content, title, author)
+    head = "\n".join(page_head(f"Edit: {title}"))
     preview = head + preview
     return preview
 
-def edit_page():
-    return f"<pre> {data} </pre>"
+def fn_check(fn):
+    print(fn)
+    if " " in fn:
+        fn = fn.replace(" ", "_")
+    fn = re.sub(r"[^a-zA-Z0-9_]", "", fn)
+    print(fn)
+    return fn
+
+def publish(title, content, author=None):
+    data = request.form
+    title = title[:20]
+    content = content.strip()[:10000]
+    if "\r" in content:
+        content = content.replace("\r", "")
+    author = author.strip()[:25]
+    page = ["<pre>"]
+    page.append(f"title: {title}")
+    page.append(f"content: {content}")
+    page.append(f"author: {author}")
+
+    fn = title + ".txt"
+    revisions = []
+
+    pages = os.listdir("pages")
+    for f in pages:
+        if fn + "." in f:
+            revisions.append(f)
+    page.append(str(revisions))
+    fnr = fn + "." + str(len(revisions))
+    page.append(f"Revision: {fnr}")
+    with open(f"pages/{fnr}", "w") as rev:
+        rev.write(content)
+    with open(f"pages/{fn}", "w") as rev:
+        rev.write(content)
+    return "\n".join(page)
